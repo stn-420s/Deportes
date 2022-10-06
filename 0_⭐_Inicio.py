90e200f6745b2a9e234e63fa1b5fbb3a8673e449
stn-420s <114024857+stn-420s@users.noreply.github.com>
2022-10-06 12:14:41 -0500

    Add files via upload

--- 0_⭐_Inicio.py
+++ 0_⭐_Inicio.py
@@ -7,16 +7,18 @@ Created on Sat Sep 24 15:08:21 2022
 # Contents of ~/my_app/streamlit_app.py
 import streamlit as st
 st.set_page_config(
-    page_title='multipaginas app',
+    page_title='DEPORTES COLOMBIA',
     page_icon='👽'
     )
 st.markdown("<h1 style = 'text-align:center;color:red;'>🏆 DEPORTES EN COLOMBIA🥇🏅</h1>", unsafe_allow_html=True)
 
-st.sidebar.success('SELECCIONE UNA PAGÍNA')
+st.sidebar.success('NO PAIN NO GAIN')
 
 from PIL import Image
-image = Image.open('deportes.jpg')
-st.image(image, caption='DEP')
+
+image2= Image.open('dep.jpg')
+
+st.image(image2)    
 st.markdown("")
-st.markdown("<h5 style = 'text-align:center;color:gray;'><b>El deporte es de suma importancia, ya que genera bienestar, tiene tanto beneficios fisicos, mentales y sociales, por esto es de gran importancia fomentar las practicas deportivas. El deporte es una herramienta de transformacion social y una actividad formadora, ya que desarrolla un papel como promotor de integracion social y desarrollo economico, en diferentes contextos geograficos, culturales y politicos.' </h5>", unsafe_allow_html=True)
+st.markdown("<h5 style = 'text-align:center;color:gray;'><b>El deporte es de suma importancia en el desarrollo de las poblaciones debido a que proporciona beneficios físicos, mentales y sociales, por lo que incentivar estas prácticas debe de ser una prioridad. El deporte es una herramienta de transformación social y una actividad formadora que desarrolla un papel de integración social y desarrollo económico en diferentes contextos  como lo son geográficos, culturales y políticos.</h5>", unsafe_allow_html=True)
 st.markdown("<h4 style = 'text-align:center;color:gray;'><b>Elaborado por:<b> Sebastian Alzate, Luis Mejia, Sara Delgado y Juan Sebastian Jaramillo </h4>", unsafe_allow_html=True)
